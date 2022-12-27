7b14983ec145c85a450cf0aefada3d977fc549cc
BK <sdsf1225@gmail.com>
2022-12-27 04:13:15 +0000

    fix:Fix preprocessing.py

--- input/code/preprocessing.py
+++ input/code/preprocessing.py
@@ -3,22 +3,27 @@ from functools import reduce
 
 
 def main():
-    # genre
-    genres_df = pd.read_csv("../data/train/genres.tsv", sep="\t")
-    array, index = pd.factorize(genres_df["genre"])
-    genres_df["genre"] = array
-    genres_df.groupby("item")["genre"].apply(list).to_json(
-        "data/Ml_item2attributes.json"
-    )
     
-    years = pd.read_csv('~/input/data/train/years.tsv', sep = '\t')
-    directors = pd.read_csv('~/input/data/train/directors.tsv', sep = '\t')
-    genres = pd.read_csv('~/input/data/train/genres.tsv', sep = '\t')
-    # titles = pd.read_csv('~/input/data/train/titles.tsv', sep = '\t')
-    writers = pd.read_csv('~/input/data/train/writers.tsv', sep = '\t')
+    print("Select the way you want to preprocess")
+    way = input("1: default(genre only) 2: select features you want to exclude")
     
+    # genre only
+    def default() : 
+        genres_df = pd.read_csv("../data/train/genres.tsv", sep="\t")
+        array, index = pd.factorize(genres_df["genre"])
+        genres_df["genre"] = array
+        genres_df.groupby("item")["genre"].apply(list).to_json(
+            "data/Ml_item2attributes.json"
+        )
+    
+        
     # 제외하고 싶은 feature를 선택해서 item2attributes.json 파일 생성
     def select_features(feature=None) : # default = all features
+        years = pd.read_csv('~/input/data/train/years.tsv', sep = '\t')
+        directors = pd.read_csv('~/input/data/train/directors.tsv', sep = '\t')
+        genres = pd.read_csv('~/input/data/train/genres.tsv', sep = '\t')
+        # titles = pd.read_csv('~/input/data/train/titles.tsv', sep = '\t')
+        writers = pd.read_csv('~/input/data/train/writers.tsv', sep = '\t')
         features = [years, directors, genres, writers]
         
         if feature != None : 
@@ -46,7 +51,13 @@ def main():
             "data/ML_item2attributes_all.json"
         )
         
-    select_features()
+    if way == 1 : 
+        print("preprocessing in default")
+        default()
+    elif way == 2 : 
+        feature = input("select features you want to exclude").split()
+        print("preprocessing in selective way")
+        select_features(feature)
 
 
 if __name__ == "__main__":
